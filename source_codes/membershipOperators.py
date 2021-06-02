847dc0239c12eed4cfad7514342f6fa3e935e4db
KhomZ <mgrprinc37@gmail.com>
2021-06-03 01:00:56 +0545

    added next py file

---
+++ source_codes/membershipOperators.py
@@ -0,0 +1,20 @@
+# Membership Operators:
+# Algorithms develop and become optimized over time as a result of constant evolution and the need to 
+# find the most efficient solutions for underlying problems in different domains.
+
+
+# These Operators cand be used with any iterable data structure in Python, including Strings, Lists, and Tuples.
+
+
+
+# -> in :- returns True if the given element is a part of the structure
+
+op1 = 'apple' in ['orange', 'apple', 'grape']
+print(op1)
+
+
+
+# -> not in :- returns True if the given element is not a part of the structure.
+
+op2 = 't' in 'stackabuse'
+print(op2)
