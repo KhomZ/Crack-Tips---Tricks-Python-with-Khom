22a7fcf709d305501cd42adc358dc4d2a51fd764
Khom <mgrprinc37@gmail.com>
2022-01-31 21:06:39 +0545

    ikhom: finding distance between two places using python| pip install geopy

---
+++ source_codes/Geopy/distance-calculate.py
@@ -0,0 +1,21 @@
+# @author: KhomZ
+from turtle import distance
+from geopy.distance import geodesic
+
+# loading the latitude and longitude from two places
+first_place = (52.2296756, 21.0122287)
+second_place = (52.406374, 16.9251681)
+
+
+# print distance in km
+distance = (geodesic(first_place, second_place).km)
+print(distance,"kilometer")
+
+
+
+'''
+output:
+279.35290160430094 kilometer
+
+'''
+
