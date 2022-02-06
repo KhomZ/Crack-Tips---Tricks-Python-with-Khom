9136b32126692645c4bcc7cebf8a1187a3bd8f06
Khom <mgrprinc37@gmail.com>
2022-02-06 22:43:07 +0545

    ikhom: Udemy Crash Course

---
+++ Udemy Python Crash Course/Python/python2.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  6 21:35:28 2022
+
+@author: KhomZ
+"""
+
+a = 10
+
+a = 20.7
+
+a = "code warriors"
+
+
+a = [1, 'code', 20.7, '100']
+
+a = {"name":"code"}
+
+a = {"name":800}
+
+
+x = 10
+y = 10
+z = 10
+
+x = y = z = 10
+
+
+
+# 1x = 10   # invalid syntax
+
+
---
+++ Udemy Python Crash Course/Python/python3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  6 21:50:11 2022
+
+@author: KhomZ
+"""
+
+x = 10/6
+y = x +6
+
+
+
+str_1 = 'code Warriors'
+
+
